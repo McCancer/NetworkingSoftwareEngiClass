528f45db0062876a10f7bfdd5087a123f75e0315
Matthew C Kunzer <matthewkunzer@gmail.com>
2017-11-20 12:23:20 -0600

    d

---
+++ NetworkingCode/WebGet.java
@@ -0,0 +1,50 @@
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Write a description of class WebGet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WebGet
+{
+    public static void main(String[] args)throws IOException
+    {
+        String host;
+        String resource; 
+        if(args.length == 2){
+            host = args[0];
+            resource = args[1];
+        }//end of if
+        else{
+            System.out.println("getting / from horstmann.com");
+            host = "horstmann.com";
+            resource = "/";
+        }//end of else
+        //Opens socket
+        final int HTTP_PORT = 80;
+        try(Socket s = new Socket(host, HTTP_PORT)){
+            //Get stream
+            InputStream instream = s.getInputStream();
+            OutputStream outstream = s.getOutputStream();
+            //turn streams into scanner and writers
+            Scanner in = new Scanner(instream);
+            PrintWriter out = new PrintWriter(outstream);
+            //send command
+            String Command = "GET " + resource + "HTTP/1.1\n"
+            + "Host: " + host + "\n\n";
+            out.print(Command);
+            out.flush();
+            //read server response
+            while(in.hasNextLine()){
+                String input = in.nextLine();
+                System.out.println(input);
+            }//end of while loop
+        }//end of try statement Closes the socket
+    }//end of main
+}//end of class
